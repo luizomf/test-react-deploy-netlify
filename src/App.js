95bc0b867adbdf04e54bae45387dad16937d8e79
Luiz Otávio <luizomf@gmail.com>
2020-01-04 01:07:46 -0300

    Github Pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import About from './pages/About';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
